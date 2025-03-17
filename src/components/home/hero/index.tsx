8f3e014737027a28b47f9ef2f6760a4b164f31d6
charanm927 <charanm927@gmail.com>
2025-03-17 16:57:48 +0530

    chore: add grid ui in home page

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Spotlight } from '@/components/SpotLight'
 import { Button } from '@/components/ui/button'
+import { cn } from '@/utils/util'
 
 export function HeroSection() {
   return (
@@ -10,6 +11,15 @@ export function HeroSection() {
         className='-top-40 left-0 md:-top-20 md:left-60'
         fill='#7F55E2'
       />
+      <div
+        className={cn(
+          'absolute inset-0',
+          '[background-size:40px_40px]',
+          '[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]',
+        )}
+      />
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      
       <div className='relative z-10 mx-auto w-full p-4 pt-28'>
         <h1 className='bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl'>
           Effortless <br /> Deployments in Minutes
@@ -25,14 +35,12 @@ export function HeroSection() {
             </Button>
             <Button
               variant={'outline'}
-              className='hover:bg-card hover:text-foreground'
-            >
+              className='hover:bg-card hover:text-foreground'>
               <svg
                 role='img'
                 className='h-6 w-6 fill-white'
                 viewBox='0 0 24 24'
-                xmlns='http://www.w3.org/2000/svg'
-              >
+                xmlns='http://www.w3.org/2000/svg'>
                 <path d='M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z'></path>
               </svg>
               Discord
