32e2e820e5420161a1a00911a19d9033a511c7b0
charanm927 <charanm927@gmail.com>
2025-03-18 14:42:05 +0530

    chore: updated dokflow data in tabs

--- src/components/home/tabs/index.tsx
+++ src/components/home/tabs/index.tsx
@@ -1,7 +1,10 @@
 'use client'
 
-import tab_1 from '/public/images/tab/tab_1.png'
-import tab_2 from '/public/images/tab/tab_2.png'
+import database from '/public/images/tab/database.png'
+import domains from '/public/images/tab/domains.png'
+import github from '/public/images/tab/github.png'
+import logs from '/public/images/tab/logs.png'
+import servers from '/public/images/tab/servers.png'
 import Image, { StaticImageData } from 'next/image'
 
 import { Tabs } from '@/components/Tabs'
@@ -9,59 +12,81 @@ import { Tabs } from '@/components/Tabs'
 export function TabsSection() {
   const tabs = [
     {
-      title: 'Product',
-      value: 'product',
+      title: 'Servers',
+      value: 'Servers',
       content: (
-        <div className='relative h-full w-full overflow-hidden rounded-md bg-gradient-to-br from-purple-700 to-violet-900 p-10 text-xl font-bold text-white md:text-4xl'>
-          <p>Product Tab</p>
-          <DummyContent src={tab_1} />
+        <div className='relative h-full w-full overflow-hidden rounded-md bg-gradient-to-br from-purple-700 to-violet-900 p-10'>
+          <p className='text-xl font-bold md:text-4xl'>Servers</p>
+          <p>
+            Easily manage and deploy your servers with scalable infrastructure
+            and automated configurations.
+          </p>
+          <DummyContent src={servers} />
         </div>
       ),
     },
     {
-      title: 'Services',
-      value: 'services',
+      title: 'Databases',
+      value: 'Databases',
       content: (
-        <div className='relative h-full w-full overflow-hidden rounded-md bg-gradient-to-br from-purple-700 to-violet-900 p-10 text-xl font-bold text-white md:text-4xl'>
-          <p>Services tab</p>
-          <DummyContent src={tab_2} />
+        <div className='relative h-full w-full overflow-hidden rounded-md bg-gradient-to-br from-purple-700 to-violet-900 p-10'>
+          <p className='text-xl font-bold md:text-4xl'>Databases</p>
+          <p>
+            Deploy, monitor, and optimize databases with seamless integrations
+            and high availability.
+          </p>
+          <DummyContent src={database} />
         </div>
       ),
     },
     {
-      title: 'Playground',
-      value: 'playground',
+      title: 'GitHub Deploy',
+      value: 'GitHub Deploy',
       content: (
-        <div className='relative h-full w-full overflow-hidden rounded-md bg-gradient-to-br from-purple-700 to-violet-900 p-10 text-xl font-bold text-white md:text-4xl'>
-          <p>Playground tab</p>
-          <DummyContent />
+        <div className='relative h-full w-full overflow-hidden rounded-md bg-gradient-to-br from-purple-700 to-violet-900 p-10'>
+          <p className='text-xl font-bold md:text-4xl'>GitHub Deploy</p>
+          <p>
+            Deploy your GitHub applications effortlessly with automated builds
+            and CI/CD pipelines.
+          </p>
+          <DummyContent src={github} />
         </div>
       ),
     },
     {
-      title: 'Content',
-      value: 'content',
+      title: 'Domains',
+      value: 'Domains',
       content: (
-        <div className='relative h-full w-full overflow-hidden rounded-md bg-gradient-to-br from-purple-700 to-violet-900 p-10 text-xl font-bold text-white md:text-4xl'>
-          <p>Content tab</p>
-          <DummyContent />
+        <div className='relative h-full w-full overflow-hidden rounded-md bg-gradient-to-br from-purple-700 to-violet-900 p-10'>
+          <p className='text-xl font-bold md:text-4xl'>Domains</p>
+          <p>
+            Simplify domain management with DNS configuration, SSL setup, and
+            seamless routing.
+          </p>
+          <DummyContent src={domains} />
         </div>
       ),
     },
     {
-      title: 'Random',
-      value: 'random',
+      title: 'Logs',
+      value: 'Logs',
       content: (
-        <div className='relative h-full w-full overflow-hidden rounded-md bg-gradient-to-br from-purple-700 to-violet-900 p-10 text-xl font-bold text-white md:text-4xl'>
-          <p>Random tab</p>
-          <DummyContent />
+        <div className='relative h-full w-full overflow-hidden rounded-md bg-gradient-to-br from-purple-700 to-violet-900 p-10'>
+          <p className='text-xl font-bold md:text-4xl'>Logs</p>
+          <p>
+            Track real-time logs for deployments, server activities, and
+            debugging insights in one place.
+          </p>
+          <DummyContent src={logs} />
         </div>
       ),
     },
   ]
 
   return (
-    <div id='tabs' className={`relative mx-auto flex h-[20rem] w-full max-w-5xl flex-col py-28 items-start justify-start [perspective:1000px] md:h-[54rem]`}>
+    <div
+      id='tabs'
+      className={`relative mx-auto flex h-[20rem] w-full max-w-5xl flex-col items-start justify-start py-28 [perspective:1000px] md:h-[58rem]`}>
       <Tabs tabs={tabs} />
     </div>
   )
@@ -76,7 +101,7 @@ const DummyContent = ({ src }: { src?: StaticImageData | string }) => {
       alt='dummy image'
       width={1000}
       height={1000}
-      className='absolute inset-x-0 -bottom-10 mx-auto h-[60%] w-[90%] rounded-md object-cover object-left-top md:h-[90%]'
+      className='absolute inset-x-0 -bottom-16 mx-auto h-[60%] w-[90%] rounded-md object-cover object-left-top md:h-[90%]'
     />
   )
 }
