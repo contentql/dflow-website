ae2c4a009c431ab5ad7a460700f7edc859315a44
charanm927 <charanm927@gmail.com>
2025-03-24 11:31:47 +0530

    chore: autoPlay and updated video src

--- src/components/home/video/index.tsx
+++ src/components/home/video/index.tsx
@@ -1,23 +1,19 @@
 import { VideoDialog } from "@/components/common/VideoDialog";
 
 export function VideoDialogSection() {
+  const videoId = "uOGmj7RWZWY"; // Extracted from the videoSrc
+  const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+
   return (
     <div className="relative max-w-7xl mx-auto px-4 py-20 md:px-6 lg:px-8">
-         <h1 className='bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text pb-10 text-center text-4xl font-bold text-transparent md:text-7xl'>
-         Watch & Deploy: Get Started with Dokflow
+      <h1 className='bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text pb-10 text-center text-4xl font-bold text-transparent md:text-7xl'>
+        Watch & Deploy: Get Started with Dokflow
       </h1>
       <VideoDialog
-        className="block dark:hidden"
+        className="block"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
-        thumbnailAlt="Hero Video"
-      />
-      <VideoDialog
-        className="hidden dark:block"
-        animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+        videoSrc={`https://www.youtube.com/embed/${videoId}?si=goLbw3rxKE2qh4qC`}
+        thumbnailSrc={thumbnailUrl}
         thumbnailAlt="Hero Video"
       />
     </div>
