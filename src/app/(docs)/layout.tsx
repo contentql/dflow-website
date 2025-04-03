5f659237c22ba4fe69de6840bab2d77f0141a989
lokesh mvs <lokeshmvs21@gmail.com>
2025-04-03 17:40:58 +0530

    fix: added light mode colors

--- src/app/(docs)/layout.tsx
+++ src/app/(docs)/layout.tsx
@@ -1,13 +1,17 @@
+import '../globals.css'
 import { RootProvider } from 'fumadocs-ui/provider'
-import 'fumadocs-ui/style.css'
 import type { ReactNode } from 'react'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body>
         <RootProvider
-          theme={{ enabled: true, defaultTheme: 'dark', enableSystem: true }}>
+          theme={{
+            enabled: true,
+            enableSystem: true,
+            defaultTheme: 'dark',
+          }}>
           {children}
         </RootProvider>
       </body>
